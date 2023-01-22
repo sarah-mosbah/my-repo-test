70ce49a95a225e1df5192757ed1d0f1286b31e45
sarah-mosbah <sarah@checkmehealth.com>
2023-01-22 12:31:22 +0200

    dd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiGatewayV1: "http://13.95.0.178/api",
   apiGatewayV2: "http://13.95.0.178/api-v2/api",
   facebookId: '539492704876653',
-  appleBundleId: 'com.checkmeweb'
+  appleBundleId: 'com.checkmeweb.signin'
 };
